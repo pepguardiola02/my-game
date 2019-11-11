4b53021c42412008cc819c164fdf4de0ff36abb9
Amar <amar.karic@skola.botkyrka.se>
2019-11-11 11:30:35 +0100

    clean up

--- lista.py
+++
@@ -1,5 +0,0 @@
-print("Här är de tre världens bästa fotbollspelare")
-topthree = ["Zlatan", "Amar", "Ronaldo"]
-print(topthree[1])
-print(topthree[0])
-print(topthree[2])
--- myndig.py
+++
@@ -1,7 +0,0 @@
-age = int(input("Hur gammal är du?")) 
-
-if age < 18:
-    print("Du är inte myndig")
-
-else:
-    print("Du är myndig")
